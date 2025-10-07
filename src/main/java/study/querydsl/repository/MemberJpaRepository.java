474e47c21bbc98c01a87db6ba0b23957d2f3a76d
juho <leeiopd@gmail.com>
2025-10-07 20:23:25 +0900

    6.실무 활용 - 순수 JPA와 Querydsl - 31.동적 쿼리와 성능 최적화 조회 - Where 절 파라미터 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -2,6 +2,8 @@ package study.querydsl.repository;
 
 import ch.qos.logback.core.util.StringUtil;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
@@ -72,7 +74,7 @@ public class MemberJpaRepository {
             builder.and(member.username.eq(condition.getUsername()));
         }
         if(hasText(condition.getTeamName())){
-            builder.and(team.name.eq(condition.getTeamName()))
+            builder.and(team.name.eq(condition.getTeamName()));
         }
         if(condition.getAgeGoe() != null){
             builder.and(member.age.gt(condition.getAgeGoe()));
@@ -91,8 +93,42 @@ public class MemberJpaRepository {
                 ))
                 .from(member)
                 .leftJoin(member.team, team)
+                .where(builder)
                 .fetch();
     }
 
+    public List<MemberTeamDto> search(MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username)? member.username.eq(username):null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName)? member.username.eq(teamName):null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null? member.age.gt(ageGoe):null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null? member.age.lt(ageLoe):null;
+    }
 
 }
